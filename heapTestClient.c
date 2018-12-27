6df7d761e2c704232b917f1e0d785952b45c2611
Fernando Zegada <ferzeg3420@gmail.com>
2018-12-27 11:00:12 -0400

    Add files via upload

--- heapTestClient.c
+++ heapTestClient.c
@@ -4,52 +4,51 @@
 int main(int argc, char* argv[])
 {
    int i;
-   int array[6] = {5, 6, 7, 8, 9, 10 };
+   int array[14] = {16, 6, 12, 12, 18, 10, 11, 7, 14, 13, 15, 5, 17, 9};
+   int carray[1] = {100};
    int length = sizeof(array) / sizeof(int);
-   printf("The length of this array: %d\n", length); // should be 5.
-   
+   printf("The length of this array: %d\n", length); // should be 14.
+   int *error = NULL;
    int *barray = NULL;
-   Heap A = newHeap(6, array);
- 
+   Heap A = newHeap(14, 14,  array);
+   Heap B = NULL;
+
    printf("\nTesting parent, left, and right.\n");
- 
-   for( int i = 1; i <= 5;  i++ )
+   for( int i = 1; i <= 4;  i++ )
    {
       printf("The parent of index %d is %d\n", i, parent(i));
-   }
-
-   for( int i = 1; i <= 5;  i++ )
-   {
       printf("The left child of index %d is %d\n", i, left(i));
+      printf("The right child of index %d is %d\n\n", i, right(i));
    }
-
-   for( int i = 1; i <= 5;  i++ )
-   {
-      printf("The right child of index %d is %d\n", i, right(i));
-   }
-
    printf("\nTesting heapify\n");
-   for( int i = 1; i <= 5;  i++ )
+   for( int i = 1; i <= length;  i++ )
    {
-      heapify(A, i); 
+      heapify(A, i);
    }
-
-   //buildHeap(A);
    printf("\nTesting heapSort()\n");
-   barray = heapSort(array, 6);
+   barray = heapSort(array, 14);
 
-   //  printing Heap
+   // printing Heap
    printf("\n\nPrinting sorted barray:\n"); // debug
-   for( int i = 1; i <= 6;  i++ )
+   for( int i = 1; i <= length;  i++ )
    {
       printf("Index %d : %d\n", i, barray[i]); // debug
    }
+   barray = heapSort(carray, 1);
+
+   // printing Heap
+   printf("\n\nPrinting sorted barray:\n"); // debug
+   for( int i = 1; i <= 1;  i++ )
+   {
+      printf("Index %d : %d\n", i, barray[i]); // debug
+   }
+
 
    printf("\nPriorityQueue Methods tests: --------------------------\n");
 
    printf("Original state of the heap.\n");
    printHeap(A);
-   
+
    printf("\nPriorityQueue heapDecreaseKey() tests -------------------\n");
 
    heapDecreaseKey(A, 6, 3);
@@ -68,56 +67,85 @@ int main(int argc, char* argv[])
    printf("Heap after decreasing key.\n");
    printHeap(A);
 
-
    printf("\n");
    printf("The minimum again is: %d\n\n", heapMinimum(A));
 
    printf("\nPriorityQueue heapExtractMin() tests -------------------\n");
 
+   printf("Printing what should be the original heap:\n");
+   printHeap(A);
+
    int minimum = heapExtractMin(A);
    printf("The minimum is %d\n", minimum);
 
-   minimum = heapExtractMin(A);
-   printf("The minimum is %d\n", minimum);
-
-   minimum = heapExtractMin(A);
-   printf("The minimum is %d\n", minimum);
-
-   minimum = heapExtractMin(A);
-   printf("The minimum is %d\n", minimum);
-
-   minimum = heapExtractMin(A);
-   printf("The minimum is %d\n", minimum);   
-
-   minimum = heapExtractMin(A);
-   printf("The minimum is %d\n", minimum);
-   
-   printf("Printing what should be an empty heap:\n");   
+   for( int i = 2; i <= 14; i++)
+   {
+      minimum = heapExtractMin(A);
+      printf("\nThe minimum is %d\n", minimum);
+      printHeap(A);
+   }
+   printf("Printing what should be an empty heap:\n");
    printHeap(A);
 
    printf("\nPriorityQueue heapInsert() tests -------------------\n");
 
-   heapInsert(A, 3);
-   printf("\nInserted one element and printing:\n");
-   printHeap(A);
-   printf("\n");
-
-   heapInsert(A, 5);
-   printf("\nInserted one element and printing:\n");
-   printHeap(A);
-   printf("\n");
-   
+   for( int i = 3; i < 14; i += 2)
+   {
+      heapInsert(A, i);
+      printf("\nInserted one element and printing:\n");
+      printHeap(A);
+      printf("\n");
+   }
+   for( int i = 2; i <= 14; i += 2)
+   {
+      heapInsert(A, i);
+      printf("\nInserted one element and printing:\n");
+      printHeap(A);
+      printf("\n");
+   }
    heapInsert(A, 1);
    printf("\nInserted one element and printing:\n");
    printHeap(A);
    printf("\n");
 
-   heapInsert(A, 4);
-   printf("\nInserted one element and printing:\n");
-   printHeap(A);
-   printf("\n");
+   printf("\nPriorityQueue testing error messages -------------------\n");
 
+   /* freeHeap(&B); */
 
+    heapify( A, 0);
+   /* heapify( A, 14); */
+   /* heapify( A, 7); */
+   /* heapify( A, 0); */
+
+   //   buildHeap(B);
+
+   for( int i = 1; i <= 14; i++) // making the heap be empty.
+   {
+      heapExtractMin(A);
+   }
+   /* heapSort(error, 1000); */
+   /* heapSort(array, 0); */
+
+   /* heapMinimum(B); */
+   /* heapMinimum(A); */
+
+   /* heapDeleteMin(B); */
+   /* heapDeleteMin(A); */
+
+   /* heapExtractMin(B); */
+   /* heapExtractMin(A); */
+
+   heapInsert(A, 10);
+
+   /* heapDecreaseKey(B, 0, 10); // more tests are possible. */
+   /* heapDecreaseKey(B, 1, 10); // more tests are possible. */
+   /* heapDecreaseKey(A, 2, 10); // more tests are possible. */
+
+   /* heapInsert(B, 10); */
+   /* heapInsert(B, 25); */
+
+   printf("Ping!\n");
+   
    freeHeap(&A);
    return(0);
 }
